5e26a28b7242412379268916b36ff2e545a2b7c0
João Pedro Albuquerque Maranhão Marinho <jpamm@cin.ufpe.br>
2022-11-10 17:40:35 -0300

    all functions

--- tes_video.py
+++ tes_video.py
@@ -1,13 +1,20 @@
 import cv2
+import numpy as np
+
+kernel = np.ones((5,5), np.uint8)
 
 img = cv2.imread("photos\gatinho-shrek.jpg")
 imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 imgBlur = cv2.GaussianBlur(imgGray, (7,7), 0)
 imgCanny = cv2.Canny(img, 100, 100)
+imgDilated = cv2.dilate(imgCanny, kernel, iterations = 1)
+imgEroded = cv2.erode(imgDilated, kernel, iterations = 1)
 
 cv2.imshow("Image", img)
 cv2.imshow("blur Image", imgGray)
 cv2.imshow("canny Image", imgCanny)
+cv2.imshow("dilated Image", imgDilated)
+cv2.imshow("eroded Image", imgEroded)
 cv2.waitKey(0)
 
 # cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
